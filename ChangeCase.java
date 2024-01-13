0558f53a12ab698a9e9930bcf60edc6d0ecf052a
Debayan Pal <paldebayan19982016@gmail.com>
2024-01-13 18:04:57 +0530

    chaning the case

--- ChangeCase.java
+++ ChangeCase.java
@@ -1,8 +1,32 @@
 import java.io.*;
+import java.util.*;
 
 public class ChangeCase {
 
     public static void main(String args[]) {
-        System.out.println("deba");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter String");
+
+        String s = sc.nextLine();
+        String s1 = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int ascii_ch = (int) ch;
+
+            if (ascii_ch >= 65 && ascii_ch <= 90) {
+                ch = Character.toLowerCase(ch);
+            }
+
+            else if (ascii_ch >= 97 && ascii_ch <= 122) {
+                ch = Character.toUpperCase(ch);
+            }
+
+            s1 = s1 + ch;
+
+        }
+
+        System.out.println(s1);
     }
 }
